d3ffd0468f5e58d67168de4dffb9b2fa996054e1
Jacobwasbeast <jacobwasbeast@outlook.com>
2025-08-25 21:13:35 -0500

    feat: SixSidedImageBlock cloning

--- common/src/main/java/net/jacobwasbeast/picaxe/recipe/ModRecipes.java
+++ common/src/main/java/net/jacobwasbeast/picaxe/recipe/ModRecipes.java
@@ -7,14 +7,30 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 
 public class ModRecipes {
     public static RecipeSerializer<ImageShieldDecorationRecipe> IMAGE_SHIELD_RECIPE_SERIALIZER;
+    public static RecipeSerializer<SixSidedImageBlockCloneRecipe> SIX_SIDED_IMAGE_CLONE_RECIPE_SERIALIZER;
+    public static RecipeSerializer<SixSidedImageBlockLightRecipe> SIX_SIDED_IMAGE_LIGHT_RECIPE_SERIALIZER;
 
     public static void initialize(BalmRecipes recipes) {
         IMAGE_SHIELD_RECIPE_SERIALIZER = new CustomRecipe.Serializer<>(
                 ImageShieldDecorationRecipe::new
         );
+        SIX_SIDED_IMAGE_CLONE_RECIPE_SERIALIZER = new CustomRecipe.Serializer<>(
+                SixSidedImageBlockCloneRecipe::new
+        );
+        SIX_SIDED_IMAGE_LIGHT_RECIPE_SERIALIZER = new CustomRecipe.Serializer<>(
+                SixSidedImageBlockLightRecipe::new
+        );
         recipes.registerRecipeSerializer(
                 () -> IMAGE_SHIELD_RECIPE_SERIALIZER,
                 PictureAxe.id("crafting_special_imageshielddecoration")
+        );
+        recipes.registerRecipeSerializer(
+                () -> SIX_SIDED_IMAGE_CLONE_RECIPE_SERIALIZER,
+                PictureAxe.id("crafting_special_sixsidedimageclone")
+        );
+        recipes.registerRecipeSerializer(
+                () -> SIX_SIDED_IMAGE_LIGHT_RECIPE_SERIALIZER,
+                PictureAxe.id("crafting_special_sixsidedimageglowstone")
         );
     }
 }
